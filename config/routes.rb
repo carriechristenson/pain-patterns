d34e70bd549ecc761c92cc836b59a6d4d079f57e
Carrie Christenson <christenson.carrie@gmail.com>
2017-01-15 13:57:27 -0800

    admin auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'Patient', at: 'auth'
 
-  devise_for :patients
+  mount_devise_token_auth_for 'Admin', at: 'admin_auth'
+  as :admin do
+    # Define routes for Admin within this block.
+  end
+
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
